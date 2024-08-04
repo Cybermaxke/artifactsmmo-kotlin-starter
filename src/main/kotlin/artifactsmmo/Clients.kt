4ec6536753a510ef8ec2b57c0007bb0d72ad82ee
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2024-08-04 13:16:05 +0200

    Use ok http client to reduce latency + retry character locked exceptions

--- src/main/kotlin/artifactsmmo/Clients.kt
+++ src/main/kotlin/artifactsmmo/Clients.kt
@@ -3,13 +3,19 @@ package artifactsmmo
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import feign.AsyncClient
+import feign.Response
+import feign.RetryableException
 import feign.Target
+import feign.codec.ErrorDecoder
 import feign.jackson.JacksonDecoder
 import feign.jackson.JacksonEncoder
 import feign.kotlin.CoroutineFeign
+import feign.okhttp.OkHttpClient
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Executors
 import kotlin.reflect.KClass
 
 private object Clients
@@ -22,6 +28,9 @@ private fun initFeign(): CoroutineFeign<Unit> {
   val objectMapper = ObjectMapper()
     .registerModules(JavaTimeModule())
     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+  val defaultErrorDecoder = ErrorDecoder.Default()
+  val okHttpClient = okhttp3.OkHttpClient.Builder().build()
+  val executor = Executors.newVirtualThreadPerTaskExecutor()
   return CoroutineFeign.builder<Unit>()
     .decoder(JacksonDecoder(objectMapper))
     .encoder(JacksonEncoder(objectMapper))
@@ -31,8 +40,28 @@ private fun initFeign(): CoroutineFeign<Unit> {
         template.header("Content-Type", "application/json")
       }
     }
+    .errorDecoder { methodKey, response ->
+      val exception = defaultErrorDecoder.decode(methodKey, response)
+      when {
+        exception is RetryableException -> exception
+        // Character is locked, also retry this
+        response.status() == 486 -> exception.retryable(response, 50)
+        else -> exception
+      }
+    }
+    .client(AsyncClient.Default(OkHttpClient(okHttpClient), executor))
     .build()
 }
+
+private fun Exception.retryable(response: Response, retryAfterMillis: Long) =
+  RetryableException(
+    response.status(),
+    message,
+    response.request().httpMethod(),
+    this,
+    retryAfterMillis,
+    response.request()
+  )
 
 private const val baseUrl = "https://api.artifactsmmo.com"
 private val feign = initFeign()
